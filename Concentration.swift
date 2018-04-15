22a88fc3882ab5db422d4edd1fb4d55399441543
Luis Carbuccia <lcarbuccia@gmail.com>
2018-04-15 10:54:25 -0400

    Lecture 4 code
    Made Card.swift conform to Hashable
    Make identifier property private

--- Concentration.swift
+++ Concentration.swift
@@ -50,7 +50,7 @@ class Concentration {
         if !cards[index].isMatched{
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index{
                 // check if cards match
-                if cards[matchIndex].identifier == cards[index].identifier {
+                if cards[matchIndex] == cards[index] {
                     cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
                     score += 2
